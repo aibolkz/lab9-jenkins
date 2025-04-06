fa3b9a6bbf1e131873ceb385c66f23ba2679f626
Aibol K <aibol.kemeldinov@colorado.edu>
2025-04-06 17:34:21 -0600

    Update test_netman_lab9.py

--- test_netman_lab9.py
+++ test_netman_lab9.py
@@ -3,8 +3,6 @@
 
 import unittest
 from ncclient import manager
-from netaddr import IPAddress
-import ipaddress
 import subprocess
 
 
@@ -15,16 +13,16 @@ class TestNetmanLab9(unittest.TestCase):
         self.r3_ip = "198.51.100.130"
         self.username = "admin"
         self.password = "admin"
-        self.r2_loopback = "10.1.2.1"
         self.r5_loopback = "10.1.5.1"
 
-    def connect_and_filter(self, host, cli_filter):
-        filter_xml = f"""
-        <filter>
-          <config-format-text-block>
-            <text-filter-spec>{cli_filter}</text-filter-spec>
-          </config-format-text-block>
-        </filter>
+    def send_rpc_cli(self, host, cli_command):
+        """Отправка CLI-команды через NETCONF <cli><cmd>"""
+        cli_rpc = f"""
+        <rpc message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+          <cli>
+            <cmd>{cli_command}</cmd>
+          </cli>
+        </rpc>
         """
         with manager.connect(
             host=host,
@@ -36,21 +34,21 @@ class TestNetmanLab9(unittest.TestCase):
             allow_agent=False,
             look_for_keys=False
         ) as conn:
-            return str(conn.get_config('running', filter_xml))
+            return str(conn.dispatch(cli_rpc))
 
     def test_loopback99_on_r3(self):
         """Loopback99 должен быть 10.1.3.1/24 на R3"""
-        output = self.connect_and_filter(self.r3_ip, "interface Loopback99")
+        output = self.send_rpc_cli(self.r3_ip, "show run interface Loopback99")
         self.assertIn("ip address 10.1.3.1 255.255.255.0", output)
 
     def test_single_ospf_area_on_r1(self):
         """Проверка, что на R1 одна OSPF area"""
-        output = self.connect_and_filter(self.r1_ip, "router ospf 1")
-        areas = [line for line in output.splitlines() if "area" in line]
-        self.assertEqual(len(areas), 1)
+        output = self.send_rpc_cli(self.r1_ip, "show run | section ospf")
+        area_count = output.count("area")
+        self.assertEqual(area_count, 1)
 
-    def test_ping_from_r2_to_r5(self):
-        """Ping с loopback R2 в loopback R5 должен пройти"""
+    def test_ping_r2_to_r5(self):
+        """Ping от Loopback R2 до Loopback R5"""
         result = subprocess.run(
             ["ping", "-c", "3", self.r5_loopback],
             stdout=subprocess.PIPE,
